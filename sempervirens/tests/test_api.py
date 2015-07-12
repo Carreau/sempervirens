5be8ac18bdae602a1b4e7675091870ecb5dd8279
Émilien Schultz <emilien.schultz@gmail.com>
2015-07-12 22:27:43 +0200

    test bug

--- sempervirens/tests/test_api.py
+++ sempervirens/tests/test_api.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from sempervirens import sempervirens as sv
 
 def test_has_accepted():
-    assert sv.has_accepted() == False
+    _pref = False
+    assert (sv.has_accepted() == False)
     sv.record_pref(True)
-    assert sv.has_accepted() == True
+    assert (sv.has_accepted() == True)
